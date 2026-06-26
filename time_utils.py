e6238938551385f6c2dfc42a531acd9f470055b7
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-26 21:17:10 +0330

    Update time_utils.py

--- time_utils.py
+++ time_utils.py
@@ -1,15 +1,9 @@
 import jdatetime
 from datetime import datetime
-import pytz
 
 
 def get_persian_datetime():
-    tehran = pytz.timezone("Asia/Tehran")
-    now = datetime.now(tehran)
-
+    now = datetime.now()
     jnow = jdatetime.datetime.fromgregorian(datetime=now)
 
-    date = jnow.strftime("%Y/%m/%d")
-    time = now.strftime("%H:%M")
-
-    return date, time
+    return jnow.strftime("%Y/%m/%d"), now.strftime("%H:%M")
