b3ecd9a1e91ac205d0435fb789229ec43f1eb149
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-26 19:39:00 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-print("Hello GitHub Actions!")
+import os
+import asyncio
+from telegram import Bot
+
+TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL = os.environ["CHANNEL_ID"]
+
+bot = Bot(token=TOKEN)
+
+async def main():
+    await bot.send_message(
+        chat_id=CHANNEL,
+        text="✅ ربات با موفقیت از GitHub Actions اجرا شد!"
+    )
+
+asyncio.run(main())
