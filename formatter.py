f2c9ba2f07bcd0f5fc37a9d51e7f1fca195f72f3
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-27 20:19:55 +0330

    Create formatter.py

---
+++ formatter.py
@@ -0,0 +1,35 @@
+from time_utils import get_persian_datetime
+
+
+def comma(num):
+
+    try:
+        return f"{int(str(num).replace(',','')):,}"
+    except:
+        return num
+
+
+def market_message(data):
+
+    date,time=get_persian_datetime()
+
+    return f"""
+📊 بازار ارز و طلا
+
+💵 دلار
+{comma(data["usd"])} ریال
+
+💶 یورو
+{comma(data["eur"])} ریال
+
+🥇 طلای ۱۸
+{comma(data["gold18"])} ریال
+
+🪙 سکه
+{comma(data["coin"])} ریال
+
+📅 {date}
+🕒 {time}
+
+@goldenhook2026
+"""
