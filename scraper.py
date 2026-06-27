2948510e4e08230106456ad9e6305915aa696d20
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-27 11:17:24 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/137.0 Safari/537.36",
+    "Accept-Language": "fa-IR,fa;q=0.9,en;q=0.8"
+}
 
 
 def get_price(url):
     try:
-        r = requests.get(url, headers=HEADERS, timeout=10)
+        r = requests.get(
+            url,
+            headers=HEADERS,
+            timeout=20
+        )
+
+        print("URL:", url)
+        print("Status:", r.status_code)
+
         soup = BeautifulSoup(r.text, "html.parser")
 
-        tag = soup.select_one('span[data-col="info.last_trade.PDrCotVal"]')
+        tag = soup.select_one(
+            'span[data-col="info.last_trade.PDrCotVal"]'
+        )
 
         if tag:
-            return tag.text.strip()
+            value = tag.get_text(strip=True)
+            print("Value:", value)
+            return value
 
+        print("Price Not Found")
         return "ERROR"
 
-    except:
+    except Exception as e:
+        print(e)
         return "ERROR"
 
 
