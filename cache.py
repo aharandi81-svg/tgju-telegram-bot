ce17e86f95eb3a79bcb1947f293189200a92f39f
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-28 10:54:01 +0330

    Create cache.py

---
+++ cache.py
@@ -0,0 +1,23 @@
+import json
+
+CACHE_FILE = "cache.json"
+
+
+def load():
+    try:
+        with open(CACHE_FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:
+        return {}
+
+
+def save(data):
+    with open(CACHE_FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
