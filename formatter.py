449346958494b054bc1e162cc4e9766586581cdc
aharandi81-svg <a.harandi.81@gmail.com>
2026-06-30 11:03:02 +0330

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -9,22 +9,39 @@ def comma(num):
         return num
 
 
-def market_message(data):
+def trend(change):
 
-    date,time=get_persian_datetime()
+    if change["diff"] > 0:
+        return f"🟢 +{change['diff']:,} (+{change['percent']}%)"
+
+    if change["diff"] < 0:
+        return f"🔴 {change['diff']:,} ({change['percent']}%)"
+
+    return "➖ بدون تغییر"
+
+
+def market_message(data, changes):
+
+    date, time = get_persian_datetime()
 
     return f"""
 📊 قیمت لحظه ای بازار
 
 💵 دلار
 {comma(data["usd"])} ریال
+{trend(changes["usd"])}
+
 💶 یورو
 {comma(data["eur"])} ریال
+{trend(changes["eur"])}
 
 🥇 طلای ۱۸
 {comma(data["gold18"])} ریال
+{trend(changes["gold18"])}
+
 🪙 سکه
 {comma(data["coin"])} ریال
+{trend(changes["coin"])}
 
 📅 {date}
 🕒 {time}
