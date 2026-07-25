32165d351035f3adc32b949b80bc2a8996984ccc
aharandi81-svg <a.harandi.81@gmail.com>
2026-07-25 23:22:20 +0330

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,50 +1,72 @@
+import re
+
+
 def to_number(value):
 
-    try:
-
-        return float(str(value).replace(",", ""))
-
-    except:
-
+    if value is None:
         return None
+
+    if isinstance(value, (int, float)):
+        return float(value)
+
+    value = str(value)
+
+    value = value.replace(",", "")
+
+    m = re.search(r"-?\d+(\.\d+)?", value)
+
+    if not m:
+        return None
+
+    return float(m.group())
 
 
 def compare_prices(current, previous):
 
-    result = {}
+    changes = {}
+
     changed = False
 
-    for key in current:
+    keys = set(current.keys()) | set(previous.keys())
 
-        cur = to_number(current.get(key))
+    for key in keys:
+
         old = to_number(previous.get(key))
 
-        if cur is None or old is None:
+        new = to_number(current.get(key))
 
-            result[key] = {
-                "changed": False,
+        if old is None or new is None:
+
+            changes[key] = {
+                "old": old,
+                "new": new,
                 "diff": 0,
                 "percent": 0,
+                "changed": False
             }
 
             continue
 
-        diff = round(cur - old, 2)
+        diff = new - old
 
         percent = 0
 
         if old != 0:
 
-            percent = round((diff / old) * 100, 2)
+            percent = (diff / old) * 100
 
-        result[key] = {
-            "changed": diff != 0,
-            "diff": diff,
-            "percent": percent,
-        }
+        is_changed = abs(diff) > 0.000001
 
-        if diff != 0:
+        if is_changed:
 
             changed = True
 
-    return changed, result
+        changes[key] = {
+            "old": old,
+            "new": new,
+            "diff": round(diff, 2),
+            "percent": round(percent, 2),
+            "changed": is_changed
+        }
+
+    return changed, changes
