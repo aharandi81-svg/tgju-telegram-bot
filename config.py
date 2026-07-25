66f4bcf97593c3df836f0208c471dcd29ef32934
aharandi81-svg <a.harandi.81@gmail.com>
2026-07-25 23:24:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,17 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNELS = [
     os.getenv("CHANNEL_ID")
 ]
+
+GIST_ID = os.getenv("GIST_ID")
+GIST_TOKEN = os.getenv("GIST_TOKEN")
+
+TGJU_URL = "https://www.tgju.org"
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/138.0.0.0 Safari/537.36"
+    ),
+    "Accept-Language": "en-US,en;q=0.9"
+}
