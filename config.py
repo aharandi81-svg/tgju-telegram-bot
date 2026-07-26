86a92d88c2c894b431f6527db8a24039edb1e4a4
aharandi81-svg <a.harandi.81@gmail.com>
2026-07-26 23:53:23 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,26 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
-CHANNELS = [
-    os.getenv("CHANNEL_ID")
-]
+CHANNEL_ID = os.getenv("CHANNEL_ID", "")
 
-GIST_ID = os.getenv("GIST_ID")
-GIST_TOKEN = os.getenv("GIST_TOKEN")
+GIST_ID = os.getenv("GIST_ID", "")
 
-TGJU_URL = "https://www.tgju.org"
+GIST_TOKEN = os.getenv("GIST_TOKEN", "")
+
+PORT = int(os.getenv("PORT", "10000"))
+
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "")
+
+TGJU_URL = "https://www.tgju.org/"
 
 HEADERS = {
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/138.0.0.0 Safari/537.36"
-    ),
-    "Accept-Language": "en-US,en;q=0.9"
+        "AppleWebKit/537.36 "
+        "(KHTML, like Gecko) "
+        "Chrome/138.0 Safari/537.36"
+    )
 }
+
+CHANNELS = [CHANNEL_ID] if CHANNEL_ID else []
